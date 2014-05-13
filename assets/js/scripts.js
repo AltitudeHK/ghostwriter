292048171fd333b45dcd7ff8d4fed5a46ca44286
jyek <justinyek@gmail.com>
2014-05-13 12:15:52 -0700

    Updated theme

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -28,8 +28,8 @@ jQuery(function($) {
     var $postIndex = $('#post-index');
 
     // Initially hide the index and show the latest post
-    $latestPost.show();
-    $postIndex.hide();
+    $latestPost.hide();
+    $postIndex.show();
 
     // Show the index if the url has "page" in it (a simple
     // way of checking if we're on a paginated page.)
